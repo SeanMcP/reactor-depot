44d66460ad0d5bab5d84d165b0409cdf7483e2ec
Sean McPherson <seanmcp25@gmail.com>
2017-09-21 18:35:58 -0400

    Create data; render data in Fuel component

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const data = {
+  fuel: [
+    {
+      name: 'Plutonium',
+      price: '999.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Uranium',
+      price: '1099.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Francium',
+      price: '1199.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ],
+  parts: [
+    {
+      name: 'Reactor',
+      price: '9999.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Steam Stack',
+      price: '4999.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Water Reservoir',
+      price: '7499.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ],
+  cleanup: [
+    {
+      name: 'HAZMAT Suit',
+      price: '199.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Lead-lined Gloves',
+      price: '99.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Nuclear Waste Barrels',
+      price: '499.99',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ]
+}
+export default data

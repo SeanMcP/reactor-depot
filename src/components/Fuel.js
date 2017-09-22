20ca5d0280f61ffa398435d446812b18c76c995c
Sean McPherson <seanmcp25@gmail.com>
2017-09-21 22:09:44 -0400

    Fix product links

--- src/components/Fuel.js
+++ src/components/Fuel.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default class Fuel extends Component {
   render() {
+    console.log('this.props.match: ', this.props.match)
     let fuelRender = data.fuel.map(item => {
       return (
         <div key={item.id} className='card p-3 m-3'>
@@ -11,8 +12,7 @@ export default class Fuel extends Component {
             <h4 className='card-title'>{item.name}</h4>
             <h5 className='card-title'>{item.price}</h5>
             <p className='card-text'>{item.desc}</p>
-            // FIXME: This link is broken
-            <Link to='`/{this.props.match}/{this.id}`'>More info</Link>
+            <Link to={`${this.props.match.url}/${item.id}`}>More info</Link>
           </div>
         </div>
       )

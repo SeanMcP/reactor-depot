c911f25f8bf4f0aa995068a49a43dd37851bf42b
Sean McPherson <seanmcp25@gmail.com>
2017-09-23 12:40:26 -0400

    Update content on Home and About components

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,19 @@ export default class About extends Component {
   render() {
     return (
       <main className='p-3'>
-        <h4 className="display-4">About</h4>
-        <p className=''>Blah blah blah</p>
+        <h4 className='display-4'>About</h4>
+        <p className='lead'>It all started with two guys and a garage</p>
+        <img className='float-right rounded ml-4 mb-4' alt='Hewlitt Packard' src='https://i.pinimg.com/736x/77/17/63/771763c3cb784edc4a52deab4af26ef5--bill-hewlett-hewlett-packard.jpg' style={{width: '33.333%'}}/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <blockquote className='blockquote'>
+          <p className='mb-0'>What began as a hobby turned into a passion</p>
+          <footer className='blockquote-footer'>Richard Manhattan in <cite title="Source Title">Don't Overreact (HarperCollins, 2006)</cite></footer>
+        </blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </main>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,8 @@ export default class Home extends Component {
           <h1 className='display-3'>We are Reactor Depot</h1>
           <p className='lead'>Reactor Depot is the leading source for nuclear products and materials around the world.</p>
           <hr className='my-4'/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <p>Reactor Depot is trending in:</p>
-          <ul>
-            <li>North Korea</li>
-            <li>Iran</li>
-          </ul>
+          <p>From fussion to fission, Reactor Depot prides itself in providing the highest quality nuclear products at competitive prices. Whether you are exploring alternative energy sources or just looking to build your arsenal, Reactor Depot the one-stop-shop for all your nuclear needs.</p>
+          <p>Reactor Depot has been feature in: The North Korea Times (PRK) and The Tehran Times (IRN)</p>
           <p className='lead'>
             <Link className='btn btn-primary btn-lg' to={'/about'} role='button'>Learn more</Link>
           </p>
